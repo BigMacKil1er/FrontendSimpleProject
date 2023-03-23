884fed388e29666b7cc37122ba9e98611f89dbdf
BigmacKiller <varik_varibrus@mail.ru>
2023-03-23 23:39:22 +0300

    I changed the text animation
    A text now appers on the left

--- script/script.js
+++ script/script.js
@@ -62,8 +62,8 @@ ScrollReveal({
 });
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .education-container, .experience-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'right' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'left' });
 
 // TYPED JS
 
