37faa3bf174676803863bb87ce302f781cadd75f
BigmacKiller <varik_varibrus@mail.ru>
2023-04-09 11:09:48 +0300

    Добавил парочку компонентов в общем то бесмысленно

--- Created using Vue.js/project_using_vue/src/components/PostList.vue
+++ Created using Vue.js/project_using_vue/src/components/PostList.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
+    <div v-if="posts.length > 0">
         <h3>Список пользователей</h3>
         <post-item 
         v-for="post in posts" 
-        :post="post">
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)">
         </post-item>
-    </div>   
+    </div>
+    <h2 v-else style="color: red">Список пуст</h2>
 </template>
 
 <script>
---
+++ Created using Vue.js/project_using_vue/src/components/UI/index.js
@@ -0,0 +1,9 @@
+import MyButton from "@/components/UI/MyButton";
+import myInput from "@/components/UI/MyInput.vue";
+import myDialog from "@/components/UI/MyDialog.vue";
+
+export default [
+    MyButton,
+    myInput,
+    myDialog
+]
